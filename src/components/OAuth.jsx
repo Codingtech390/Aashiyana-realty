256f896a2746ac2435ca978e85cb3adf9a4d4e7f
codingtech <codingtech390@gmail.com>
2024-05-31 21:07:13 +0530

    Completed OAuth functionality for the sign up page.

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { toast } from "react-toastify";
+
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { useNavigate } from "react-router";
 import { FcGoogle } from "react-icons/fc";
 
 export default function OAuth() {
+  const navigate = useNavigate();
+
+  async function onGoogleSignup() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Check if the user is already in the database if not add the user to the database.
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+    } catch (error) {
+      toast.error("Couldn't authorize with Google Signup!");
+      console.log(error);
+    }
+  }
   return (
-    <button className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg rounded transition duration-200 ease-in-out">
+    <button
+      type="button"
+      onClick={onGoogleSignup}
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg rounded transition duration-200 ease-in-out"
+    >
       <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
       Continue with Google
     </button>
